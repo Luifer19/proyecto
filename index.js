c53a652e5ddde2ba4f667c282146c595cf47ebcd
luisa gomez <luisafernandauribegomez11@gmail.com>
2024-04-25 17:21:21 -0500

    añadimos la ruta productos y pintamos productos en  el index.html

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
+const cors=require("cors")
 const port = 3000
-
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/productos', (req, res) => {
+  const productos=[
+   {nombre:"producto1",precio:110},
+   {nombre:"producto2",precio:95},
+   {nombre:"producto3",precio:95},
+   {nombre:"producto4",precio:95},
+   {nombre:"producto5",precio:95}
+  ]
+  res.json(productos)
 })
 
 app.listen(port, () => {
