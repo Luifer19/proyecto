10987d86056683d500ba1528e6e457cda207e727
luisa gomez <luisafernandauribegomez11@gmail.com>
2024-05-01 16:53:20 -0500

    creamos la base de datos y añadimos productos, se conecto el backend con la base de datos.

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'proyectotienda'
+});
+ 
+connection.connect();
+
 app.get('/productos', (req, res) => {
   const productos=[
-   {nombre:"producto1",precio:110},
-   {nombre:"producto2",precio:95},
-   {nombre:"producto3",precio:95},
+   {nombre:"producto1",precio:110,url:"https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202012/04/00120610001263____6__600x600.jpg"},
+   {nombre:"producto2",precio:95,url:"https://123cuidatuhogar.com/wp-content/uploads/2019/07/Lavaloza-500-ml.png"},
+   {nombre:"producto13",precio:95},
    {nombre:"producto4",precio:95},
    {nombre:"producto5",precio:95}
   ]
