a9d0442165a88bc7a09014025b19661f668d6669
luisa gomez <luisafernandauribegomez11@gmail.com>
2024-05-15 16:06:44 -0500

    Se añadieron los productos a la base de datos y se pintaron

--- index.js
+++ index.js
@@ -7,26 +7,29 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
+const mysql      = require('mysql2/promise');
+const connection = mysql.createPool({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'proyectotienda'
 });
  
-connection.connect();
 
-app.get('/productos', (req, res) => {
-  const productos=[
-   {nombre:"producto1",precio:110,url:"https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202012/04/00120610001263____6__600x600.jpg"},
-   {nombre:"producto2",precio:95,url:"https://123cuidatuhogar.com/wp-content/uploads/2019/07/Lavaloza-500-ml.png"},
-   {nombre:"producto13",precio:95},
-   {nombre:"producto4",precio:95},
-   {nombre:"producto5",precio:95}
-  ]
-  res.json(productos)
+app.get('/productos', async(req, res) => {
+  try {
+    const [results, fields] = await connection.query(
+      'SELECT * FROM `productos`'
+    );
+    res.json(results)
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  } catch (err) {
+    console.log(err);
+  }
+  
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
